afba17c899e6fbaadd7a695c1f84639e330806f8
kira roberts and micah zaayer <micah@>
2019-08-14 11:07:36 -0700

    add backend logic and change submit listener to click listener.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business Logic
+
 var add = function(number1, number2) {
   return number1 + number2
 };
@@ -14,9 +16,34 @@ var divide = function(number1, number2) {
   return number1 / number2
 }
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+//  Front End Logic
 
-var result = divide(number1, number2)
+$(document).ready(function() {
+  $("#add").click(function() {
+  var number1 = parseInt($("#input1").val());
+  var number2 = parseInt($("#input2").val());
+  var sum = add(number1, number2);
+  $("#output").text(sum);
+  });
 
-alert(divide(number1, number2));
+  $("#subtract").click(function()  {
+  var number1 = parseInt($("#input1").val());
+  var number2 = parseInt($("#input2").val());
+  var difference = subtract(number1, number2);
+  $("#output").text(difference);
+  });
+
+  $("#multiply").click(function()  {
+  var number1 = parseInt($("#input1").val());
+  var number2 = parseInt($("#input2").val());
+  var product = multiply(number1, number2);
+  $("#output").text(product);
+  });
+
+  $("#divide").click(function()  {
+  var number1 = parseInt($("#input1").val());
+  var number2 = parseInt($("#input2").val());
+  var quotient = divide(number1, number2);
+  $("#output").text(quotient);
+  });
+});
